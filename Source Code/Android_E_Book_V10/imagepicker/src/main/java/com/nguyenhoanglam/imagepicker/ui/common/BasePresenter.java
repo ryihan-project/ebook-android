dd35a0807ad2a692113e2381792ea10ce2a6ff8d
raihan <ryihan55@gmail.com>
2021-05-20 06:00:02 +0600

    Update BasePresenter.java

--- Source Code/Android_E_Book_V10/imagepicker/src/main/java/com/nguyenhoanglam/imagepicker/ui/common/BasePresenter.java
+++ Source Code/Android_E_Book_V10/imagepicker/src/main/java/com/nguyenhoanglam/imagepicker/ui/common/BasePresenter.java
@@ -4,6 +4,10 @@ package com.nguyenhoanglam.imagepicker.ui.common;
  * Created by hoanglam on 8/17/17.
  */
 
+    public void detachView() {
+        view = null;
+    }
+
     protected boolean isViewAttached() {
         return view != null;
     }
