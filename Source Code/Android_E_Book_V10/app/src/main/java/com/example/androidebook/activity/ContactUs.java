7731e52457a1e72b4c076a6a3bb54c1ca1437e68
raihan <ryihan55@gmail.com>
2021-05-28 10:31:50 +0600

    Create ContactUs.java

---
+++ Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/activity/ContactUs.java
@@ -0,0 +1,13 @@
+package com.example.androidebook.activity;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+public class ContactUs extends AppCompatActivity {
+
+}
