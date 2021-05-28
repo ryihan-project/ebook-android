1735e399c08a8017b24dd3f822c32619f5874144
raihan <ryihan55@gmail.com>
2021-05-28 10:30:45 +0600

    Update MainActivity.java

--- Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/activity/MainActivity.java
+++ Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/activity/MainActivity.java
@@ -5,3 +5,5 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+}
