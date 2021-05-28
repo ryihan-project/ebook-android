526c96f3713120e61737dbbcfb667f28457136d6
raihan <ryihan55@gmail.com>
2021-05-28 10:29:30 +0600

    Create MainActivity.java

---
+++ Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/activity/MainActivity.java
@@ -0,0 +1,7 @@
+package com.example.androidebook.activity;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
