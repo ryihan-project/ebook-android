3dc40da8f3a98b72e4ba0fb42696b6fbf959fb70
raihan <ryihan55@gmail.com>
2021-05-28 10:29:57 +0600

    Create PrivacyPolicy.java

---
+++ Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/activity/PrivacyPolicy.java
@@ -0,0 +1,11 @@
+package com.example.androidebook.activity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
