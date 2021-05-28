cad427932d4cf0e8d2b58e66f7c67eda2388a8fd
raihan <ryihan55@gmail.com>
2021-05-28 10:30:28 +0600

    Update PDFShow.java

--- Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/activity/PDFShow.java
+++ Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/activity/PDFShow.java
@@ -4,3 +4,5 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+public class PDFShow extends AppCompatActivity implements OnPageChangeListener, OnLoadCompleteListener, OnPageErrorListener {
+}
