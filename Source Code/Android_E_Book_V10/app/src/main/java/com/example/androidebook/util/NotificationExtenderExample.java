65699070271f238569d10a9323a57976d2f44381
raihan <ryihan55@gmail.com>
2021-05-30 21:53:08 +0600

    Update NotificationExtenderExample.java

--- Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/util/NotificationExtenderExample.java
+++ Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/util/NotificationExtenderExample.java
@@ -8,3 +8,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+public class NotificationExtenderExample extends NotificationExtenderService {
+
+    private NotificationManager mNotificationManager;
+    private String NOTIFICATION_CHANNEL_ID = "android_e_book";
+    private String message, bigpicture, title, id, subId, type, titleName, url;
+
+}
