93e18680fff389e01cac33c5454a8aa8dcd9f594
raihan <ryihan55@gmail.com>
2021-05-30 21:53:17 +0600

    Update NotificationExtenderExample.java

--- Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/util/NotificationExtenderExample.java
+++ Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/util/NotificationExtenderExample.java
@@ -14,5 +14,18 @@ public class NotificationExtenderExample extends NotificationExtenderService {
     private String NOTIFICATION_CHANNEL_ID = "android_e_book";
     private String message, bigpicture, title, id, subId, type, titleName, url;
 
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            // Log exception
+            return null;
+        }
     }
 }
