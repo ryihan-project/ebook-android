70c2a9635b0423c9898420f8f6c0c731d275f058
raihan <ryihan55@gmail.com>
2021-05-30 21:52:56 +0600

    Create NotificationExtenderExample.java

---
+++ Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/util/NotificationExtenderExample.java
@@ -0,0 +1,10 @@
+package com.example.androidebook.util;
+
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
