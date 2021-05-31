bab237e957198e09324d3ac00c2e4e142d635fd9
raihan <ryihan55@gmail.com>
2021-05-31 17:44:48 +0600

    Update EndlessRecyclerViewScrollListener.java

--- Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/util/EndlessRecyclerViewScrollListener.java
+++ Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/util/EndlessRecyclerViewScrollListener.java
@@ -2,3 +2,16 @@ package com.example.androidebook.util;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+public abstract class EndlessRecyclerViewScrollListener extends RecyclerView.OnScrollListener {
+
+    // The minimum amount of items to have below your current scroll position
+    // before loading more.
+
+    // Call this method whenever performing new searches
+    public void resetState() {
+        this.currentPage = this.startingPageIndex;
+        this.previousTotalItemCount = 0;
+        this.loading = true;
+    }
+}
