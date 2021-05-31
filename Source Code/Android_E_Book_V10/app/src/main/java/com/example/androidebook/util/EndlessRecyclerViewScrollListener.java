b854ce5e69d7bde4e6890ced3af28337b546c226
raihan <ryihan55@gmail.com>
2021-05-31 17:44:53 +0600

    Update EndlessRecyclerViewScrollListener.java

--- Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/util/EndlessRecyclerViewScrollListener.java
+++ Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/util/EndlessRecyclerViewScrollListener.java
@@ -8,6 +8,18 @@ public abstract class EndlessRecyclerViewScrollListener extends RecyclerView.OnS
     // The minimum amount of items to have below your current scroll position
     // before loading more.
 
+        // If it isn’t currently loading, we check to see if we have breached
+        // the visibleThreshold and need to reload more data.
+        // If we do need to reload some more data, we execute onLoadMore to fetch the data.
+        if (!loading && totalItemCount <= (lastVisibleItem + visibleThreshold)) {
+            currentPage++;
+            onLoadMore(currentPage, totalItemCount);
+            loading = true;
+        }
+    }
+
+    public abstract void onLoadMore(int page, int totalItemsCount);
+
     // Call this method whenever performing new searches
     public void resetState() {
         this.currentPage = this.startingPageIndex;
