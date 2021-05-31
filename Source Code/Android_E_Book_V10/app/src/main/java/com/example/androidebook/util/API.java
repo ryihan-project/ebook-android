7e319dcf53faacd3d31dd9ed30448765077fb6fa
raihan <ryihan55@gmail.com>
2021-05-31 17:43:24 +0600

    Update API.java

--- Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/util/API.java
+++ Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/util/API.java
@@ -5,6 +5,14 @@ import android.util.Base64;
 
 import com.google.gson.annotations.SerializedName;
 public class API {
+    private int getRandomSalt() {
+        Random random = new Random();
+        return random.nextInt(900);
+    }
+
+    private String md5(String input) {
+        try {
+            // Create MD5 Hash
     }
 
 }
