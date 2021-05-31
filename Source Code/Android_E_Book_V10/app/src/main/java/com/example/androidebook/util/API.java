804f82ecd43ab96e8be90d89490af040aa28ca26
raihan <ryihan55@gmail.com>
2021-05-31 17:43:28 +0600

    Update API.java

--- Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/util/API.java
+++ Source Code/Android_E_Book_V10/app/src/main/java/com/example/androidebook/util/API.java
@@ -13,6 +13,20 @@ public class API {
     private String md5(String input) {
         try {
             // Create MD5 Hash
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            digest.update(input.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuilder hexString = new StringBuilder();
+            for (int i = 0; i < messageDigest.length; i++)
+                hexString.append(String.format("%02x", messageDigest[i]));
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
     }
 
 }
