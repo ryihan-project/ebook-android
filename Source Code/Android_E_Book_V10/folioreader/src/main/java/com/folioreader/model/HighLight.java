15807f26d7846280543913e61a9c42dcdaaede80
raihan <ryihan55@gmail.com>
2021-06-04 09:48:56 +0600

    Create HighLight.java

---
+++ Source Code/Android_E_Book_V10/folioreader/src/main/java/com/folioreader/model/HighLight.java
@@ -0,0 +1,13 @@
+package com.folioreader.model;
+
+import java.util.Date;
+
+/**
+ * Interface to access Highlight data.
+ *
+ * @author gautam chibde on 9/10/17.
+ */
+
+public interface HighLight {
+
+    /**
